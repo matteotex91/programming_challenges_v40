bb4542a7cac42c395367670b8ec5d74f10df2482
Matteo Tessarolo <matteo.tessarolo@unibas.ch>
2024-09-09 17:38:34 +0200

    Update

--- WIP/72_sudoku_solver/sudoku.py
+++ WIP/72_sudoku_solver/sudoku.py
@@ -6,6 +6,9 @@ class Sudoku:
         self.N = N
         self.data = np.zeros((N**2, N**2))
 
+    """This function computes the list of available numbers at some position.
+    """
+
     def available_number_list(self, pos):
         return [
             i
@@ -26,10 +29,10 @@ class Sudoku:
             )
         ]
 
-    """ this functions fills the map with the only numbers which are univocally determined.
+    """ This functions computes a soft solution of the sudoku, without guessing.
     It returns:
         - fill_list : list of the filled positions
-        - consistend : bool, True if the sudoku is consistent, False if not.
+        - consistent : bool, True if the sudoku is solvable or undetermined, False if errors are detected (same numbers in the same row, column or quadrant while computing the soft solution).
 
     """
 
@@ -49,13 +52,10 @@ class Sudoku:
                     return fill_list, False
         return fill_list, True
 
-    """ looks for the complete solution
-    1- look for univocally determined numbers
-    2- if not completed, looks for the most determined cell (the one with the least of available numbers)
-    3- cycle over all the available numbers. If inconsistency detected, or it's impossible to determine any solution, roll back
-    return values:
+    """ Computes the complete solution, possibly by guessing.
+    Returns:
     True -> solved
-    False -> inconsistent
+    False -> unsolvable
     """
 
     def solve(self):
@@ -67,8 +67,7 @@ class Sudoku:
         if np.sum(self.data == 0) == 0:  # found solution
             return True
         pos = np.transpose(np.where(self.data == 0))[0]
-        num_list_at_pos = self.available_number_list(pos)
-        for num in num_list_at_pos:
+        for num in self.available_number_list(pos):
             print(str(pos) + " -> " + str(num))
             self.data[*pos] = num
             solved = self.solve()
@@ -82,32 +81,17 @@ class Sudoku:
 
 if __name__ == "__main__":
     s = Sudoku()
-    # s.data = np.array(
-    #    [
-    #        [0, 7, 0, 5, 8, 3, 0, 2, 0],
-    #        [0, 5, 9, 2, 0, 0, 3, 0, 0],
-    #        [3, 4, 0, 0, 0, 6, 5, 0, 7],
-    #        [7, 9, 5, 0, 0, 0, 6, 3, 2],
-    #        [0, 0, 3, 6, 9, 7, 1, 0, 0],
-    #        [6, 8, 0, 0, 0, 2, 7, 0, 0],
-    #        [9, 1, 4, 8, 3, 5, 0, 7, 6],
-    #        [0, 3, 0, 7, 0, 1, 4, 9, 5],
-    #        [5, 6, 7, 4, 2, 9, 0, 1, 3],
-    #    ]
-    # )
-    # fill_list, consist = s.fill_determined_numbers()
-    # print(consist)
     s.data = np.array(
         [
-            [0, 0, 0, 0, 0, 3, 0, 2, 0],
-            [0, 0, 9, 2, 0, 0, 3, 0, 0],
-            [3, 0, 0, 0, 0, 0, 0, 0, 7],
-            [0, 0, 5, 0, 0, 0, 0, 3, 2],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 4, 5, 0, 0, 0, 0, 0, 0],
+            [8, 3, 0, 0, 0, 7, 4, 0, 0],
+            [0, 0, 0, 2, 0, 0, 0, 5, 0],
+            [0, 8, 4, 6, 0, 0, 5, 0, 1],
+            [2, 0, 0, 8, 3, 0, 0, 4, 0],
+            [0, 0, 0, 5, 0, 0, 0, 0, 7],
+            [3, 7, 0, 0, 0, 5, 0, 6, 0],
+            [0, 2, 0, 0, 0, 0, 0, 8, 0],
+            [5, 6, 1, 9, 0, 0, 0, 7, 0],
         ]
     )
     print(s.solve())
