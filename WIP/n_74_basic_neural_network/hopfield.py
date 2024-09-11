49526d128d53195f64d34b198538b57f707f5dcd
matteotex91 <matteotex@gmail.com>
2024-09-11 20:47:18 +0200

    Update

--- WIP/n_74_basic_neural_network/hopfield.py
+++ WIP/n_74_basic_neural_network/hopfield.py
@@ -6,18 +6,29 @@ class HopfieldNetwork:
     def __init__(self, init_patterns: np.ndarray):
         self.init_patterns = init_patterns
         self.N = init_patterns.shape[1]
-        self.W = np.tensordot(init_patterns, init_patterns, axes=0) / self.N
+        self.W = np.zeros((self.N, self.N))
+        for pattern in init_patterns:
+            self.W += np.tensordot(pattern, pattern, axes=0) / self.N
+        self.W *= np.ones((self.N, self.N)) - np.eye(self.N)
 
-    def relax_state(self, state: np.ndarray, n_iter: int):
+    def relax_state_synchro(self, state: np.ndarray, n_iter: int):
         energy_arr = []
         state_arr = []
+        overlaps = []
         for i in range(n_iter):
             energy_arr.append(-0.5 * np.dot(np.dot(self.W, state), state))
             state_arr.append(np.copy(state))
+            overlaps.append(
+                np.array([np.dot(state, pattern) for pattern in self.init_patterns])
+                / self.N
+            )
             state = np.sign(np.dot(self.W, state))
-        return state_arr, energy_arr
+        return np.array(state_arr), np.array(energy_arr), np.array(overlaps)
 
 
 if __name__ == "__main__":
-    h = HopfieldNetwork(np.array([[1, -1, -1, -1], [-1, -1, 1, -1]]))
-    s, e = h.relax_state(np.array([]))
+    h = HopfieldNetwork(
+        np.array([[1, -1, -1, -1, -1, -1, -1, -1], [-1, -1, 1, -1, -1, -1, -1, -1]])
+    )
+    s, e, o = h.relax_state_synchro(np.array([1, 1, -1, -1, -1, -1, -1, 1]), 100)
+    print("stop here")
