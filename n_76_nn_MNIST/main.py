564f99aee15c4e58daa50281e7a701ed8c4d9730
Matteo Tessarolo <matteo.tessarolo@unibas.ch>
2024-09-16 21:14:21 +0200

    Update - working mnist with tensorflow

--- n_76_nn_MNIST/main.py
+++ n_76_nn_MNIST/main.py
@@ -1,6 +1,18 @@
 import tensorflow as tf
 import numpy as np
 import os
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+)
+from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtGui import QKeyEvent, QPixmap, QPainter, QPen, QMouseEvent
+import sys
+
+LOW_TONE = 255  # was 100
+MID_TONE = 255  # was 175
+HIG_TONE = 255  # was 255
 
 
 class NeuralEngine:
@@ -79,9 +91,122 @@ class NeuralEngine:
             self.model = model
 
     def predict(self, image: np.ndarray) -> int:
-        return max(self.model.predict(np.array([image])))
+        wrapped_image = np.zeros((1, *image.shape, 1))
+        wrapped_image[0, :, :, 0] = image.T / 255.0
+        prediction = self.model.predict(wrapped_image)
+        # print(prediction)
+        return np.argmax(prediction[0])
 
 
-n = NeuralEngine(os.getcwd() + "/n_76_nn_MNIST/model/mnist_fit.h5")
-test = np.array([])
-n.predict
+class GameWindow(QMainWindow):
+    def __init__(
+        self,
+        nn_model_path: str,
+        map_shape: np.ndarray = np.array([28, 28]),
+        pixel_shape: np.ndarray = np.array([20, 20]),
+        pixel_offset: int = 2,
+    ):
+        QMainWindow.__init__(self)
+        self.neural_engine = NeuralEngine(nn_model_path)
+        self.map_shape = map_shape
+        self.map = np.zeros(map_shape)
+        self.pixel_shape = pixel_shape
+        self.pixel_offset = pixel_offset
+        self.label = QLabel()
+        self.setFixedSize(*(map_shape * pixel_shape))
+        canvas = QPixmap(*(map_shape * pixel_shape))
+        canvas.fill(Qt.lightGray)
+        self.label.setPixmap(canvas)
+        self.setCentralWidget(self.label)
+        self.redraw_game_graphics()
+        self.clicked = False
+
+    def redraw_game_graphics(self):
+        canvas = self.label.pixmap()
+        canvas.fill(Qt.lightGray)
+        painter = QPainter(canvas)
+        pen = QPen()
+        pen.setWidth(2)
+        pen.setColor(Qt.darkGray)
+        painter.setPen(pen)
+        for i in range(self.map_shape[0]):
+            for j in range(self.map_shape[1]):
+                painter.drawRect(
+                    i * self.pixel_shape[0],
+                    j * self.pixel_shape[1],
+                    self.pixel_shape[0],
+                    self.pixel_shape[1],
+                )
+                color = None
+                match self.map[i, j]:
+                    case 100:
+                        color = Qt.lightGray
+                    case 175:
+                        color = Qt.gray
+                    case 255:
+                        color = Qt.darkGray
+                if color is not None:
+                    painter.fillRect(
+                        i * self.pixel_shape[0] + self.pixel_offset,
+                        j * self.pixel_shape[1] + self.pixel_offset,
+                        self.pixel_shape[0] - 2 * self.pixel_offset,
+                        self.pixel_shape[1] - 2 * self.pixel_offset,
+                        color,
+                    )
+
+        prediction = int(self.neural_engine.predict(self.map))
+        painter.drawText(
+            QRect(
+                0 * self.pixel_shape[0] + self.pixel_offset,
+                0 * self.pixel_shape[1] + self.pixel_offset,
+                self.pixel_shape[0] - 2 * self.pixel_offset,
+                self.pixel_shape[1] - 2 * self.pixel_offset,
+            ),
+            0,
+            str(prediction),
+        )
+        painter.end()
+        self.update()
+
+    def mousePressEvent(self, a0: QMouseEvent | None) -> None:
+        self.clicked = True
+
+    def mouseReleaseEvent(self, a0: QMouseEvent | None) -> None:
+        self.clicked = False
+
+    def mouseMoveEvent(self, a0: QMouseEvent | None) -> None:
+        if self.clicked:
+            x = int(a0.x() / self.pixel_shape[0])
+            y = int(a0.y() / self.pixel_shape[1])
+            self.map[x, y] = HIG_TONE
+            if x > 0 and y > 0:
+                self.map[x - 1, y - 1] = max(LOW_TONE, self.map[x - 1, y - 1])
+            if x < 28 and y < 28:
+                self.map[x + 1, y + 1] = max(LOW_TONE, self.map[x + 1, y + 1])
+            if x > 0 and y < 28:
+                self.map[x - 1, y + 1] = max(LOW_TONE, self.map[x - 1, y + 1])
+            if x < 28 and y > 0:
+                self.map[x + 1, y - 1] = max(LOW_TONE, self.map[x + 1, y - 1])
+            if x > 0:
+                self.map[x - 1, y] = max(MID_TONE, self.map[x - 1, y])
+            if x < 28:
+                self.map[x + 1, y] = max(MID_TONE, self.map[x + 1, y])
+            if y > 0:
+                self.map[x, y - 1] = max(MID_TONE, self.map[x, y - 1])
+            if y < 28:
+                self.map[x, y + 1] = max(MID_TONE, self.map[x, y + 1])
+            self.redraw_game_graphics()
+
+    def keyReleaseEvent(self, a0: QKeyEvent | None) -> None:
+        match a0.key():
+            case Qt.Key_Space:
+                self.map = np.zeros(self.map_shape)
+                self.redraw_game_graphics()
+
+
+if __name__ == "__main__":
+    nn_model_path = os.getcwd() + "/n_76_nn_MNIST/model/mnist_fit.h5"
+    app = QApplication(sys.argv)
+    win = GameWindow(nn_model_path=nn_model_path)
+    win.show()
+    app.exec_()
--- n_76_nn_MNIST/test4.py
+++ n_76_nn_MNIST/test4.py
@@ -1,42 +1,84 @@
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtCore import Qt
+import numpy as np
+
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+)
+from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtGui import QKeyEvent, QPixmap, QPainter, QPen, QMouseEvent
 
 
-class MainWindow(QtWidgets.QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        self.label = QtWidgets.QLabel()
-        canvas = QtGui.QPixmap(56, 56)
-        canvas.fill(Qt.white)
+class GameWindow(QMainWindow):
+    def __init__(
+        self,
+        map_shape: np.ndarray = np.array([28, 28]),
+        pixel_shape: np.ndarray = np.array([25, 25]),
+        pixel_offset: int = 3,
+    ):
+        QMainWindow.__init__(self)
+        self.map_shape = map_shape
+        self.map = np.zeros(map_shape)
+        self.pixel_shape = pixel_shape
+        self.pixel_offset = pixel_offset
+        self.label = QLabel()
+        self.setFixedSize(*(map_shape * pixel_shape))
+        canvas = QPixmap(*(map_shape * pixel_shape))
+        canvas.fill(Qt.lightGray)
         self.label.setPixmap(canvas)
         self.setCentralWidget(self.label)
+        self.redraw_game_graphics()
+        self.clicked = False
 
-        self.last_x, self.last_y = None, None
-
-    def mouseMoveEvent(self, e):
-        if self.last_x is None:  # First event.
-            self.last_x = e.x()
-            self.last_y = e.y()
-            return  # Ignore the first time.
-
-        painter = QtGui.QPainter(self.label.pixmap())
-        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
+    def redraw_game_graphics(self):
+        canvas = self.label.pixmap()
+        canvas.fill(Qt.lightGray)
+        painter = QPainter(canvas)
+        pen = QPen()
+        pen.setWidth(2)
+        pen.setColor(Qt.darkGray)
+        painter.setPen(pen)
+        for i in range(self.map_shape[0]):
+            for j in range(self.map_shape[1]):
+                painter.drawRect(
+                    i * self.pixel_shape[0],
+                    j * self.pixel_shape[1],
+                    self.pixel_shape[0],
+                    self.pixel_shape[1],
+                )
+                if self.map[i, j] == 255:
+                    painter.fillRect(
+                        i * self.pixel_shape[0] + self.pixel_offset,
+                        j * self.pixel_shape[1] + self.pixel_offset,
+                        self.pixel_shape[0] - 2 * self.pixel_offset,
+                        self.pixel_shape[1] - 2 * self.pixel_offset,
+                        Qt.darkGray,
+                    )
         painter.end()
         self.update()
 
-        # Update the origin for next time.
-        self.last_x = e.x()
-        self.last_y = e.y()
+    def mousePressEvent(self, a0: QMouseEvent | None) -> None:
+        self.clicked = True
 
-    def mouseReleaseEvent(self, e):
-        self.last_x = None
-        self.last_y = None
+    def mouseReleaseEvent(self, a0: QMouseEvent | None) -> None:
+        self.clicked = False
+
+    def mouseMoveEvent(self, a0: QMouseEvent | None) -> None:
+        if self.clicked:
+            x = int(a0.x() / self.pixel_shape[0])
+            y = int(a0.y() / self.pixel_shape[1])
+            self.map[x, y] = 255
+            self.redraw_game_graphics()
+
+    def keyReleaseEvent(self, a0: QKeyEvent | None) -> None:
+        if a0.key() == Qt.Key_Space:
+            self.map = np.zeros(self.map_shape)
+            self.redraw_game_graphics()
 
 
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec_()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = GameWindow()
+    win.show()
+    app.exec_()
