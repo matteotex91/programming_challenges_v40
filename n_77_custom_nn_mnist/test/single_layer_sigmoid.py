06490771a6ba3197a0acce8f636315b53ec96793
Matteo Tessarolo <matteo.tessarolo@unibas.ch>
2024-09-23 16:12:15 +0200

    Update - 99% accuracy with single layer. Tested with 100 orthogonal patterns over 100 neurons.

--- n_77_custom_nn_mnist/test/single_layer_sigmoid.py
+++ n_77_custom_nn_mnist/test/single_layer_sigmoid.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from scipy.stats import ortho_group
+from random import shuffle
 
 """ Build a single layer nn from scratch.
 Activation function : sigmoid
@@ -41,39 +42,53 @@ def cost_gradient_W_D(W, D, x_calc_arr, y_exp_arr):
     return gradient_W, gradient_D
 
 
-size = 100
-iterations = 10000
-learning_rage = 0.001
+if __name__ == "__main__":
 
-W = np.random.random((size, size))  # generate random weights
-D = np.random.random(size)  # generate random biases
+    size = 100
+    iterations = 10000
+    learning_rage = 0.001
+    flag_train_together = False
 
-# X = [np.random.random(size) for i in range(size)]
-# Y = [row for row in np.eye(size)]
-X = ortho_group.rvs(dim=size)  # orthogonal inputs
-Y = np.eye(size)  # canonical vectors as output
-costs = []
+    W = np.random.random((size, size))  # generate random weights
+    D = np.random.random(size)  # generate random biases
 
-for i in tqdm(range(iterations)):
-    g_W, g_D = cost_gradient_W_D(W, D, X, Y)
-    W -= g_W * learning_rage
-    D -= g_D * learning_rage
-    costs.append(cost(W, D, X, Y))
+    # X = [np.random.random(size) for i in range(size)]
+    # Y = [row for row in np.eye(size)]
+    X = ortho_group.rvs(dim=size)  # orthogonal inputs
+    Y = np.eye(size)  # canonical vectors as output
+    costs = []
 
-confusion_matrix = []
-accuracy = 0.0
-for i, x in enumerate(X):
-    y = apply(W, D, x)
-    confusion_matrix.append(y)
-    if np.argmax(y) == i:
-        accuracy += 1
-    print(y)
-accuracy *= 100 / size
-print(f"accuracy : {accuracy}%")
+    if flag_train_together:
+        for i in tqdm(range(iterations)):
+            g_W, g_D = cost_gradient_W_D(W, D, X, Y)
+            W -= g_W * learning_rage
+            D -= g_D * learning_rage
+            costs.append(cost(W, D, X, Y))
+    else:
+        epochs = 6
+        for i in range(epochs):
+            print(f"epoch {i+1} over {epochs}")
+            shuffle_data = list(zip(X, Y))
+            shuffle(shuffle_data)
+            for x, y in tqdm(shuffle_data):
+                for i in range(iterations // epochs):
+                    g_W, g_D = cost_gradient_W_D(W, D, [x], [y])
+                    W -= g_W * learning_rage
+                    D -= g_D * learning_rage
 
+    confusion_matrix = []
+    accuracy = 0.0
+    for i, x in enumerate(X):
+        y = apply(W, D, x)
+        confusion_matrix.append(y)
+        if np.argmax(y) == i:
+            accuracy += 1
+        print(y)
+    accuracy *= 100 / size
+    print(f"accuracy : {accuracy}%")
 
-plt.plot(costs)
-plt.show()
+    plt.plot(costs)
+    plt.show()
 
-plt.pcolormesh(confusion_matrix)
-plt.show()
+    plt.pcolormesh(confusion_matrix)
+    plt.show()
