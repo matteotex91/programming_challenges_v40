475c628c626436215fdb91a13a97ae387929493e
Matteo Tessarolo <matteo.tessarolo@unibas.ch>
2024-10-01 15:19:26 +0200

    Update

--- n_77_custom_nn_mnist/test/double_layer_sigmoid.py
+++ n_77_custom_nn_mnist/test/double_layer_sigmoid.py
@@ -4,13 +4,13 @@ from tqdm import tqdm
 from scipy.stats import ortho_group
 
 """ Build a double layer nn from scratch.
-Activation function : analytical relu
+Activation function : sigmoid
+Still todo : train over single samples in different epochs, not collectively
 """
 
 
 def transform(W1, D1, W2, D2, X):
-    X1 = activation_function(np.dot(W1, X) + D1)
-    return activation_function(np.dot(W2, X1) + D2)
+    return activation_function(np.dot(W2, activation_function(np.dot(W1, X) + D1)) + D2)
 
 
 def activation_function(v: np.ndarray) -> np.ndarray:
@@ -21,31 +21,45 @@ def activation_function_derivative(v: np.ndarray) -> np.ndarray:
     return 1 / np.power(np.cosh(v), 2)
 
 
-def cost(W, D, x_calc_arr, y_exp_arr) -> float:
+def cost(W1, D1, W2, D2, x_calc_arr, y_exp_arr) -> float:
     cost = 0
     for x_calc, y_exp in zip(x_calc_arr, y_exp_arr):
-        y_delta = apply(W, D, x_calc) - y_exp
+        y_delta = transform(W1, D1, W2, D2, x_calc) - y_exp
         cost += 0.5 * np.dot(y_delta, y_delta)
     return cost
 
 
-def cost_gradient_W_D(W, D, x_calc_arr, y_exp_arr):
-    gradient_D = np.zeros_like(D)
-    gradient_W = np.zeros_like(W)
+def cost_gradient_W_D(W1, D1, W2, D2, x_calc_arr, y_exp_arr):
+    gradient_W1 = np.zeros_like(W1)
+    gradient_D1 = np.zeros_like(D1)
+    gradient_W2 = np.zeros_like(W2)
+    gradient_D2 = np.zeros_like(D2)
     for x_calc, y_exp in zip(x_calc_arr, y_exp_arr):
-        sigm_arg = np.dot(W, x_calc) + D
-        y_calc = sigmoid(sigm_arg)
-        gradient_D += (y_calc - y_exp) * activation_function_derivative(sigm_arg)
-        gradient_W += np.tensordot(gradient_D, x_calc, axes=0)
-    return gradient_W, gradient_D
+        y_calc = transform(W1, D1, W2, D2, x_calc)
+        y_delta = y_calc - y_exp
+        y_calc_1 = activation_function(np.dot(W1, x_calc) + D1)
+        y_1_der = activation_function_derivative(np.dot(W1, x_calc) + D1)
+        y_2_der = activation_function_derivative(
+            np.dot(W2, activation_function(np.dot(W1, x_calc) + D1)) + D2
+        )
+        gradient_W1 += np.tensordot(
+            np.dot(y_delta * y_2_der, W2) * y_1_der, x_calc, axes=0
+        )
+        gradient_D1 += np.dot(y_delta * y_2_der, W2) * y_1_der
+        gradient_W2 += np.tensordot(y_delta * y_2_der, y_calc_1, axes=0)
+        gradient_D2 += y_delta * y_2_der
+
+    return gradient_W1, gradient_D1, gradient_W2, gradient_D2
 
 
-size = 100
+size = 10
 iterations = 10000
-learning_rage = 0.001
+learning_rate = 0.001
 
-W = np.random.random((size, size))  # generate random weights
-D = np.random.random(size)  # generate random biases
+W1 = np.random.random((size, size))  # generate random weights
+D1 = np.random.random(size)  # generate random biases
+W2 = np.random.random((size, size))  # generate random weights
+D2 = np.random.random(size)  # generate random biases
 
 # X = [np.random.random(size) for i in range(size)]
 # Y = [row for row in np.eye(size)]
@@ -54,15 +68,22 @@ Y = np.eye(size)  # canonical vectors as output
 costs = []
 
 for i in tqdm(range(iterations)):
-    g_W, g_D = cost_gradient_W_D(W, D, X, Y)
-    W -= g_W * learning_rage
-    D -= g_D * learning_rage
-    costs.append(cost(W, D, X, Y))
+    (
+        g_W1,
+        g_D1,
+        g_W2,
+        g_D2,
+    ) = cost_gradient_W_D(W1, D1, W2, D2, X, Y)
+    W1 -= g_W1 * learning_rate
+    D1 -= g_D1 * learning_rate
+    W2 -= g_W2 * learning_rate
+    D2 -= g_D2 * learning_rate
+    costs.append(cost(W1, D1, W2, D2, X, Y))
 
 confusion_matrix = []
 accuracy = 0.0
 for i, x in enumerate(X):
-    y = apply(W, D, x)
+    y = transform(W1, D1, W2, D2, x)
     confusion_matrix.append(y)
     if np.argmax(y) == i:
         accuracy += 1
